ac9e10a359d2368eacbf751a54b28eb3e14e1952
ben lyman <bnlyman@gmail.com>
2015-03-25 10:40:43 -0600

    Filled in actions for the projects_controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,50 @@
 class ProjectsController < ApplicationController
-  def index
-  end
+
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def show
   end
 
+  def index
+    @projects = Project.all
+  end
+
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      redirect_to :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @event.destroy
   end
+
+  private
+  
+    def project_params
+      params.require(:project).permit(:name, :description, :due_date)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
 end
