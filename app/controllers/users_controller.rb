8e3ee19e86eea45ba7ee03929e457e7bbd20fd4f
ben lyman <bnlyman@gmail.com>
2015-03-26 16:00:17 -0600

    Initial setup of Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_user
+  
   def profile
+  
   end
 
   def show
+  
   end
 
   def index
+  
   end
+
+  private
+  	def set_user
+  		@user = current_user
+  	end
 end
