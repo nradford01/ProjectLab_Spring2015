c22b8055c6683a8b11501e37bea2fd4afa2d9fbb
ben lyman <bnlyman@gmail.com>
2015-03-26 15:24:37 -0600

    Updated routes.rb for new routes needed with the new devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'sessions'
+      }
   root 'static_pages#home'
 
   get '/about', to: 'static_pages#about', as: :about
