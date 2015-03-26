3e2760625edd1ec337bdef2e28725257db1c8453
ben lyman <bnlyman@gmail.com>
2015-03-26 17:04:20 -0600

    Added content for Users view pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,16 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
-	before_action :set_user
   
   def profile
-  
+    @user = current_user
   end
 
   def show
-  
+    @user = user.find(params[:id])
   end
 
   def index
-  
+    @users = User.all
   end
 
-  private
-  	def set_user
-  		@user = current_user
-  	end
 end
