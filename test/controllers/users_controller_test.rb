27c357b7af8bffc722ca2b9f2043aedf888e12bc
ben lyman <bnlyman@gmail.com>
2015-03-27 15:42:53 -0600

    Updated users_controller_test.rb so that all test pass

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:one)
+    @other_user = users(:two)
+  end
+
   test "should get profile" do
+    get :profile
+    assert_redirected_to new_user_session_path
+    sign_in :user, @user
     get :profile
     assert_response :success
   end
 
   test "should get show" do
-    get :show
+    sign_in @user
+    user = @other_user
+    get :show, id: user.id
     assert_response :success
   end
 
   test "should get index" do
     get :index
+    assert_redirected_to new_user_session_path
+    sign_in :user, @user
+    get :index
     assert_response :success
   end
-
 end
