cc5468200bb2307fe5c67a145b661d0fcc956166
ben lyman <bnlyman@gmail.com>
2015-03-27 10:01:52 -0600

    Fixed another problem in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
   get 'users/profile', to: 'users#profile', as: :profile
-
-  resources :users
+  get 'users/index', to: 'users#index', as: :index
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-      
+   
+  resources :users, only: [:show]
+    
   root 'static_pages#home'
 
   get '/about', to: 'static_pages#about', as: :about
