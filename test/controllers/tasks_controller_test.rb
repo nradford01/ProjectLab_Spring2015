17f113701e643785f75dce998e849e5e6ae03bac
JoeyHilton <shnek81@gmail.com>
2015-03-27 13:51:41 -0600

    task controller tests added

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,29 +1,44 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
+
+def setup
+    @task = tasks(:one)
+    @other_task = tasks(:two)
+    @new_params = { name: "Updated", description: "This is updated"}
   end
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
+
+  # test "should get edit" do
+  #   get :edit
+  #   assert_response :success
+  # end
+
+  # test "should get create" do
+  #   get :create
+  #   assert_response :success
+  # end
+
+  # test "should get update" do
+  #   get :update
+  #   assert_response :success
+  # end
+
+  # test "should get destroy" do
+  #   get :destroy
+  #   assert_response :success
+  # end
+
+  test "Tasks should belong to project when created/update" do
+
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
+  test "Deleting a project should delete all of its tasks" do
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
   end
 
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
+
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+def setup
+    @task = tasks(:one)
+  end
+
+  test "due_date should be in the future" do
+    @task.due_date = Time.now + 2.minutes
+    assert @task.valid?
+  end
+
+  test "due_date should not be in the past" do
+    @task.due_date = Time.now - 2.minutes
+    assert_not @task.valid?
+  end
+
+  test "Task fields shouldn't be able to be blank" do
+    @task.name = " "
+    @task.description = " "
+    assert_not @task.valid?
+  end
+
+  # test "Tasks should belong to project when created/update" do
+
+  # end
+
+  # test "Deleting a project should delete all of its tasks" do
+
   # end
 end
